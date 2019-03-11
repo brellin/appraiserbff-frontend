751d6b14830a5dc1d8ed87d6c651dedd08b6aff4
ajb85 <ajbrush85@gmail.com>
2019-03-11 12:57:43 -0400

    Setup index.js with imports and Redux store.  Actions framework created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// React imports
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux imports
+import { Provider } from "react-redux";
+import { createStore, applyMiddleWare } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+
+// Route imports
+import { BrowerRouter as Router, Route } from "react-router-dom";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
