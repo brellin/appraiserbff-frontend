a947185541c00db3505e7e7566db463ec1e1b2e0
ajb85 <ajbrush85@gmail.com>
2019-03-12 13:45:59 -0400

    negligible changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import "./globalStyles.scss";
 import axios from "axios";
 
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get("https://appraiser-back-end.herokuapp.com/works")
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
   render() {
     return <div className="App">Hello World</div>;
   }
