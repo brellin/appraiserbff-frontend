5075b48d9aec028a0a85f2fd37054f8ea7e2c29c
ajb85 <ajbrush85@gmail.com>
2019-03-13 20:29:47 -0400

    Minor fixes to merged conflicts

--- src/components/Cards/FullCard/FullCard.js
+++ src/components/Cards/FullCard/FullCard.js
@@ -1,52 +1,61 @@
+import React from "react";
+import styles from "./fullCard.module.scss";
+import { Link } from "react-router-dom";
 
-import React from 'react'
-import styles from './fullCard.module.scss'
-import { Link } from 'react-router-dom'
-
-import { connect } from 'react-redux'
-
-import { deleteRealEstate } from '../../../actions'
+import { connect } from "react-redux";
 import { deleteRealEstate } from "../../../actions";
 
- 
 const FullCard = props => {
-    return(
-        <div>
-            <Link to="/home/">
-                <div className={styles.cardSettings}>
-                </div>
+  return (
+    <div>
+      <Link to="/home/">
+        <div className={styles.cardSettings} />
+      </Link>
 
-            </Link>
+      <div className={styles.cardSettingsModal}>
+        <div className={styles.cardSettingsTop}>
+          <Link to="/home/">
+            <i className="fas fa-times" />
+          </Link>
+          <h2>{props.address}</h2>
+          <img src={props.picture} />
+        </div>
+        <hr />
 
-            <div className={styles.cardSettingsModal}>
-                <div className={styles.cardSettingsTop}>
-                    <Link to="/home/">
-                        <i className="fas fa-times" />
-                    </Link>
-                    <h2>{props.address}</h2>
-                    <img src={props.picture} />
-                </div>
-                <hr />
-
-                <div className={styles.bottom}>
-                    <p>bath:      <span>{props.bathrooms}</span></p>
-                    <p>bed:       <span>{props.bedrooms}</span></p>
-                    <p>built:     <span>{props.built}</span></p>
-                    <p>onMarket:  <span>{props.onMarket}</span></p>
-                    <p>zestimate: <span>{props.zestimate}</span></p>
-                </div>
-
-                <button
-                className={styles.deleteButton}
-                id={props.id}
-                onClick={e => {
-                    e.preventDefault();
-                    props.deleteRealEstate(props.id);
-                }}
-                >delete</button>
-            </div>
+        <div className={styles.bottom}>
+          <p>
+            bath: <span>{props.bathrooms}</span>
+          </p>
+          <p>
+            bed: <span>{props.bedrooms}</span>
+          </p>
+          <p>
+            built: <span>{props.built}</span>
+          </p>
+          <p>
+            onMarket: <span>{props.onMarket}</span>
+          </p>
+          <p>
+            zestimate: <span>{props.zestimate}</span>
+          </p>
         </div>
 
+        <button
+          className={styles.deleteButton}
+          id={props.id}
+          onClick={e => {
+            e.preventDefault();
+            props.deleteRealEstate(props.id);
+          }}
+        >
+          delete
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default connect(null, {deleteRealEstate})(FullCard)
-
+export default connect(
+  null,
+  { deleteRealEstate }
+)(FullCard);
